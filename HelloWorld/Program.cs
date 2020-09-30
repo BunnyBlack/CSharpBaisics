00a125346b0ebd7b469020eda2a552e9ec472f4d
qw <xyy6836@126.com>
2020-09-30 18:36:12 +0800

    "Hello World" and "Variables"

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBaisics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
