488f646c044c6f6aedd13ac293ff7c0bccb5cb69
xyy <xyy6836@126.com>
2020-10-06 16:29:38 +0800

    push test

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Variables
+﻿using System;
+
+namespace Variables
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello World");
         }
     }
 }
