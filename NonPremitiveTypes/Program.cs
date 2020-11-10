dd3f754cc8e759b7f48632d87e60dad7eba2a5cf
qw <xyy6836@126.com>
2020-11-10 23:58:22 +0800

    update code

--- NonPremitiveTypes/Program.cs
+++ NonPremitiveTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NonPremitiveTypes.Math;
 
 namespace NonPremitiveTypes
 {
@@ -6,7 +7,28 @@ namespace NonPremitiveTypes
     {
         public static void Main(string[] args)
         {
-            
+            var john = new Person {FirstName = "John", LastName = "Smith"};
+            john.Introduce();
+
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var name = new string[3] {"Jack", "John", "Mary"};
         }
     }
 }
