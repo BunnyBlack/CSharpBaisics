13478275e63ac462272b26920a96975cf02c2978
qw <xyy6836@126.com>
2020-11-12 18:15:19 +0800

    chapter 4 complete

--- NonPremitiveTypes/Program.cs
+++ NonPremitiveTypes/Program.cs
@@ -9,6 +9,14 @@ namespace NonPremitiveTypes
         RegisteredAirMail = 1,
         Express = 2
     }
+
+    public struct RGBColor
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+
     internal class Program
     {
         public static void Main(string[] args)
@@ -36,12 +44,38 @@ namespace NonPremitiveTypes
 
             var name = new string[3] {"Jack", "John", "Mary"};
 
+            var names = string.Format("{0} {1} {2}", name[0], name[1], name[2]);
+            var names1 = string.Join(", ", name);
+            Console.WriteLine(names);
+            Console.WriteLine(names1);
+
+
+            var str0 = "abc\n";
+            var str1 = @"abc\n";
+
+            Console.WriteLine(str0);
+            Console.WriteLine(str1);
+
             var method = ShippingMethod.Express;
             Console.WriteLine(method);
-            Console.WriteLine((ShippingMethod)method);
-            Console.WriteLine((int)method);
+            var methodId = 2;
+            Console.WriteLine((ShippingMethod) methodId);
+            Console.WriteLine((int) method);
             Console.WriteLine(method.ToString());
-            
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine((int) shippingMethod);
+
+            var a = 1;
+            var b = a;
+            b++;
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+
+            var arr = new int[3] {1, 1, 1};
+            var arr1 = arr;
+            arr1[0] = 0;
+            Console.WriteLine("arr[0] = {0}, arr1[0] = {1}", arr[0], arr1[0]);
         }
     }
 }
