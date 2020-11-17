9d12b01fb15c6562aa66bc564798654c835a5a06
BunnyBlack <xyy6836@126.com>
2020-11-17 18:12:41 +0800

    chapter 5 complete

---
+++ ControlFlow/Exercises.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    public class Exercises
+    {
+        public static void Exercise43_1()
+        {
+            int input;
+            Console.WriteLine("Please enter a number between 1 to 10:");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input > 0 && input < 11)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public static void Exercise43_2()
+        {
+            int number1;
+            int number2;
+            Console.WriteLine("Please enter two numbers:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            var result = number1 > number2 ? number1 : number2;
+            Console.WriteLine(string.Format("The bigger one is {0}", result));
+        }
+
+        public static void Exercise43_3()
+        {
+            int width;
+            int height;
+
+            Console.WriteLine("Please enter the width and height of a picture:");
+            width = Convert.ToInt32(Console.ReadLine());
+            height = Convert.ToInt32(Console.ReadLine());
+            var result = width > height ? "landscape" : "portrait";
+            Console.WriteLine(string.Format("The image is {0}", result));
+        }
+
+        public static void Exercise43_4()
+        {
+            int speed;
+            int speedLimit = 1;
+            Console.WriteLine("Please enter the speed of the car:");
+            speed = Convert.ToInt32(Console.ReadLine());
+            if (speed <= speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                float overSpeed = (float)((float)speed - (float)speedLimit);
+                int Quotient = (int) Math.Ceiling(overSpeed / 5);
+                if (Quotient > 12)
+                {
+                    Console.WriteLine("License Suspended!");
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("Demerit point is {0}", Quotient));
+                }
+            }
+        }
+    }
+}
--- ControlFlow/Program.cs
+++ ControlFlow/Program.cs
@@ -6,6 +6,127 @@ namespace ControlFlow
     {
         public static void Main(string[] args)
         {
+            // IfElseStatement();
+
+            // SwitchCaseStatement();
+
+            // ForStatement();
+
+            // ForeachStatement();
+
+            // RandomClass();
+            
+            // Exercises.Exercise43_1();
+            
+            // Exercises.Exercise43_2();
+            
+            // Exercises.Exercise43_3();
+            
+            // Exercises.Exercise43_4();
+        }
+
+        public static void RandomClass()
+        {
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(random.Next(1, 10));
+            }
+
+            Console.WriteLine();
+
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            for (int i = 0; i < passwordLength; i++)
+            {
+                buffer[i] = (char) ('a' + random.Next(0, 26));
+            }
+
+            var password = new string(buffer);
+            Console.WriteLine(password);
+        }
+
+        public static void ForeachStatement()
+        {
+            var name = "John Smith";
+            foreach (var ch in name)
+            {
+                Console.WriteLine(ch);
+            }
+
+            var numbers = new int[4] {1, 2, 3, 4};
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void ForStatement()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 10; i > -1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            var name = "John Smith";
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+
+        public static void SwitchCaseStatement()
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                {
+                    Console.WriteLine("It's autumn and a beautiful season.");
+                    break;
+                }
+                case Season.Summer:
+                {
+                    Console.WriteLine("It's perfect to go to beach");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+                }
+            }
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                {
+                    Console.WriteLine("We've got promotion!");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+                }
+            }
+        }
+
+        public static void IfElseStatement()
+        {
             var hour = 10;
             if (hour > 0 && hour < 12)
             {
@@ -30,8 +151,10 @@ namespace ControlFlow
             {
                 price = 29.95f;
             }
+
             price = isGoldCustomer ? 19.95f : 29.95f;
-            
+
+            Console.WriteLine(price);
         }
     }
 }
