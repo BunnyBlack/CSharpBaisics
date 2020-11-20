00714162a63934646314f5fbede4a88e98562077
BunnyBlack <xyy6836@126.com>
2020-11-20 18:34:25 +0800

    chapter7 update

---
+++ WorkingWithDates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorkingWithDates
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015,1,1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: "+ now.Hour);
+            Console.WriteLine("Minute: "+ now.Minute);
+            Console.WriteLine("Second: "+now.Second);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            
+            
+            
+        }
+    }
+}
